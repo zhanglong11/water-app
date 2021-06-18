581ad728bcabed7a6fef66c0d6b1b351053b6503
张龙 <390758246@qq.com>
2021-06-18 09:46:07 +0800

    'test'

---
+++ src/views/components/ChartBasic/Chart.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Container, Echarts} from './components';
+
+export default class App extends Component {
+  setNewOption(option) {
+    this.chart.setNewOption(option);
+  }
+
+  render() {
+    return (
+      <Container width={this.props.width} height={this.props.height}>
+        <Echarts {...this.props} ref={(e) => (this.chart = e)} />
+      </Container>
+    );
+  }
+}
